bfce1c2fd97a67f078e63d6d0523b7ede4011450
paulaNerea <panelevi@gmail.com>
2024-08-23 19:44:02 -0300

    tercer commit - agrego la class Cadeteria, faltan sus metodos

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -1,4 +1,13 @@
 public class Cadeteria
 {
+    public string Nombre { get; set; }
+    public string Telefono { get; set; }
+    public List<Cadete> ListadoCadetes { get; set; }
     
+    public Cadeteria(string nombre, string telefono)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        ListadoCadetes = new List<Cadete>();
+    }
 }
